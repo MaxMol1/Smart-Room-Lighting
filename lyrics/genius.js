b696859fe4bf8a976298b778085a47ec1e910ee6
MaxMol1 <mmolchanov@ucdavis.edu>
2020-12-11 14:37:29 -0800

    Fix limit of 10 songs per artist search

--- lyrics/genius.js
+++ lyrics/genius.js
@@ -1,26 +1,20 @@
 const fetch = require("node-fetch");
 const cheerio = require('cheerio')
 const Genius = require('genius-api');
-const { exit } = require("process");
 
 async function Main() {
-    Genius.prototype.getSongUrl = async function getSongUrl(artistName, songName) {
-        const normalizeName = name => name.replace(/[^\w]/g, '').toLowerCase()
-        const artistNameNormalized = normalizeName(artistName)
-        const songNameNormalized = normalizeName(songName)
-    
+    /* Function to get artist id */
+    Genius.prototype.getArtistID = async function getArtistID(artistName) {
         const response = await this.search(artistName)
         for (let i = 0; i < response.hits.length; i += 1) {
             const hit = response.hits[i]
-            if (hit.type === 'song' && normalizeName(hit.result.primary_artist.name) === artistNameNormalized
-               && normalizeName(hit.result.title) === songNameNormalized) {
-                console.log('')
-                return hit.result.url
-               }
+            if (hit.type === 'song' && normalizeName(hit.result.primary_artist.name) === artistName)
+                return hit.result.primary_artist.id
         }
-        throw new Error("Did not find any songs whose artist is " + artistName)
+        throw new Error("Did not find an artist called: " + artistName)
     }
     
+    /* Function to get song page HTML */
     Genius.prototype.getSongHTML = async function getSongHTML(geniusUrl) {
         const response = await fetch(geniusUrl, { method: 'GET' })
         if (response.ok) {
@@ -31,6 +25,7 @@ async function Main() {
         }
     }
 
+    /* Function to parse lyrics from HTML */
     async function parseSongHTML(htmlText) {
         const $ = cheerio.load(htmlText)
         return $('.lyrics').text()
@@ -39,19 +34,34 @@ async function Main() {
     // genius access token
     const accessToken = 'XfpcBSGdEivzTpfOBtxy6hwKgBLFOYRb5Zla2UADZp9MVVV3xBsTFZ9FUK1wwv3q'
     const genius = new Genius(accessToken)
-    const song_name = process.argv[2]
-    const artist_name = process.argv[3]
+    
+    // artist and song for which to find lyrics
+    const normalizeName = name => name.replace(/[^\w]/g, '').toLowerCase()
+    const song_name = normalizeName(process.argv[2])
+    const artist_name = normalizeName(process.argv[3])
 
-    console.log("SONG: " + song_name + " ARTIST: " + artist_name)
+    /* 1. Get artist ID using artist name */
+    const artistID = await genius.getArtistID(artist_name)
 
-    // Main routine
-    const songUrl = await genius.getSongUrl(artist_name, song_name)
+    /* 2. Get top 50 songs by artist */
+    const songs = await genius.songsByArtist(artistID, {
+        per_page: 50,
+        sort: 'popularity'
+    })
+
+    /* 3. Get correct song url from list of 50 songs  */
+    let songUrl
+    songs.songs.forEach(song => {
+        const title = song.title
+        if (normalizeName(title) === song_name)
+            songUrl = song.url
+    })
+
+    /* 4. Fetch and parse html from song url */
     const songHTML = await genius.getSongHTML(songUrl)
     var lyrics = await parseSongHTML(songHTML)
 
-    // console.log('~~~~ FETCHING LYRICS ~~~~\n')
-    // console.log('SONG      :  ', song_name)
-    // console.log('ARTIST    :  ', artist_name)
+    /* 5. Return lyrics to console */
     console.log(lyrics)
 }
 
