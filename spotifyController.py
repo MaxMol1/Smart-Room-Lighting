9e1e3897c0ef3faaebdd6f934609c82867352eed
MaxMol1 <mmolchanov@ucdavis.edu>
2021-08-28 23:47:16 -0700

    Fix commit

--- spotifyController.py
+++ spotifyController.py
@@ -2,8 +2,19 @@ import requests
 
 class SpotifyController:
     def __init__(self):
+        self.AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+        self.TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
         self.CURRENTLY_PLAYING_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing'
         self.ARTIST_ENDPOINT = 'https://api.spotify.com/v1/artists/'
+
+    def authRedirect(self, url):
+        return self.AUTH_ENDPOINT + url
+
+    def generateTokens(self, data):
+        try:
+            return requests.post(self.TOKEN_ENDPOINT, data=data).json()
+        except:
+            raise Exception('FAILED to generate Spotify access and refresh tokens')
 
     # Returns currently playing song and info
     def trackCurrentSong(self, headers):
