183b1556e4eb34d94c5a48b54bb5382d3527d3de
MaxMol1 <mmolchanov@ucdavis.edu>
2021-08-31 13:49:24 -0700

    Add robust error handling

--- geniusController.py
+++ geniusController.py
@@ -7,20 +7,50 @@ class GeniusController:
     # Returns artist id using artist name
     def getGeniusSearchRes(self, params, headers):
         try:
-            return requests.get(self.GENIUS_URL + '/search/' , params=params, headers=headers).json()
+            res = requests.get(self.GENIUS_URL + '/search/' , params=params, headers=headers)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
+            raise
         except:
-            raise Exception('FAILED to search Genius for: ' + params['q'])
+            raise Exception('FAILED to retrieve Genius search results for: ' + params['q'])
 
     # Returns an artists top songs
     def getGeniusArtistSongs(self, artistId, params, headers):
         try:
-            return requests.get(self.GENIUS_URL + '/artists/' + str(artistId) + '/songs/', params=params, headers=headers).json()
+            res = requests.get(self.GENIUS_URL + '/artists/' + str(artistId) + '/songs/', params=params, headers=headers)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
+            raise
         except:
-            raise Exception('FAILED to search Genius by artistId: ' + str(artistId))
+            raise Exception('FAILED to retrieve artist songs by artistId: ' + str(artistId))
 
     # Returns web response from a Genius song url
     def getGeniusSongUrlRes(self, url):
         try:
-            return requests.get(url)
+            res = requests.get(url)
+            res.raise_for_status()
+            return res
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
+            raise
         except:
-            raise Exception('FAILED to search Genuis by song url: ' + url)
+            raise Exception('FAILED to retrieve results from: ' + url)
--- spotifyController.py
+++ spotifyController.py
@@ -15,7 +15,17 @@ class SpotifyController:
     # Returns response from token endpoint
     def generateTokens(self, data):
         try:
-            return requests.post(self.TOKEN_ENDPOINT, data=data).json()
+            res = requests.post(self.TOKEN_ENDPOINT, data=data)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
+            raise
         except:
             raise Exception('FAILED to generate Spotify access and refresh tokens')
 
@@ -25,15 +35,31 @@ class SpotifyController:
             res = requests.get(self.CURRENTLY_PLAYING_ENDPOINT, headers=headers)
             res.raise_for_status()
             return res.json()
-        except requests.exceptions.HTTPError as e:
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
             raise
         except:
-            raise Exception('FAILED to track current playing song')
+            raise Exception('FAILED to retrieve currently playing song')
 
 
     # Returns genre of current playing song
     def trackCurrentSongGenres(self, artistId, headers):
         try:
-            return requests.get(self.ARTIST_ENDPOINT + str(artistId), headers=headers).json()
+            res = requests.get(self.ARTIST_ENDPOINT + str(artistId), headers=headers)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.ConnectionError:
+            raise
+        except requests.exceptions.Timeout:
+            raise
+        except requests.exceptions.RequestException:
+            raise
         except:
-            raise Exception('FAILED to fetch artist genres for artistId: ' + str(artistId))
+            raise Exception('FAILED to retrieve genres for artistId: ' + str(artistId))
